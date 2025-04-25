32f14770964f7eb8c7efb05b52180f03eb3a2636
Azraelbazrael <casilanazrael@gmail.com>
2025-04-25 15:39:08 -0500

    fixing missing files

--- item_test_scenes/screen_layers.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-signal on_show
-signal on_hide
-
-func _on_visibility_changed() -> void:
-	if visible == true:
-		emit_signal("on_show")
-	if visible == false:
-		emit_signal("on_hide")
