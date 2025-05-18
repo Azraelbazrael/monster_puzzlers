3554d43bb083eef68d84920726ba233e88d4deca
Azraelbazrael <casilanazrael@gmail.com>
2025-05-18 17:01:10 -0500

    added map resources

--- 00_main/level_manager.gd
+++ 00_main/level_manager.gd
@@ -2,8 +2,9 @@ extends Node
 class_name level_manager
 @export var current_map: map_resource
 
-func set_current_map(value: map_resource):
-	pass
+func _ready() -> void:
+	$"../TileMap".map = current_map
+	
 	
 ## have a function that calls from main that checks which map resource to load
 ## have functions that instantiate the enemies and items to the scene.. rolling between max and min in orfer to have a random spawn of enemies
--- dungeon_generator/tile_map.gd
+++ dungeon_generator/tile_map.gd
@@ -2,12 +2,26 @@ extends TileMap
 
 var tiles_in_room = [] ##array of tiles
 var rand_point
-	
+@export var map: map_resource: set = set_map
+
+
 func _ready() -> void:
 	Global.connect("game_over", clear_tiles)
 	Global.connect("obj_placed", place_object)
 
 
+func set_map(Map: map_resource):
+	map = Map
+	tile_set = map.tileset
+	update_map()
+
+func update_map():
+	if map is not map_resource:
+		return
+	if not is_inside_tree():
+		await ready	
+		
+		
 func clear_tiles():
 	tiles_in_room.clear()
 
